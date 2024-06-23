a2fce036673a2fdd8232bf03ae38abe290480df7
anis safia <anissafia110@gmail.com>
2024-06-23 20:53:07 +0100

    Add Social Media button share

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
+import {
+  TwitterShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterIcon,
+  FacebookIcon,
+  LinkedinIcon,
+} from "react-share";
 
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +40,45 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            gap: "4px",
+            justifyContent: "center",
+            marginTop: "10px",
+          }}
+        >
+          <TwitterShareButton
+            url={"https://www.example.com"}
+            quote={quote.content}
+            hashtag="#muo"
+          >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+          <FacebookShareButton
+            url={"https://www.example.com"}
+            quote={quote.content}
+            hashtag="#muo"
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+          <LinkedinShareButton
+            url={"https://www.example.com"}
+            quote={quote.content}
+            hashtag="#muo"
+          >
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
